a297369178d3ba7f87046709eaf6889ae3286b80
mv-per <mav.pereira@outlook.com>
2023-04-17 12:38:08 -0300

    create experiment serializer

--- adsorption_database/serializers/experiment_serializer.py
+++ adsorption_database/serializers/experiment_serializer.py
@@ -1,15 +1,21 @@
-from typing import Any, List, Optional
+from typing import Any, Dict, List
 from attr import fields
-
 import numpy as np
-from adsorption_database.models.adsorbate import Adsorbate
-from h5py import Group, SoftLink
-import numpy.typing as npt
+
+
+from adsorption_database.defaults import MONO_ISOTHERMS, MIXTURE_ISOTHERMS
+
+from h5py import Group
+
 from adsorption_database.models.adsorbent import Adsorbent
 from adsorption_database.models.experiment import Experiment
 
-from adsorption_database.models.isotherms import MixIsotherm, MonoIsotherm
+from adsorption_database.models.isotherms import Isotherm, MixIsotherm, MonoIsotherm
 from adsorption_database.serializers.abstract_serializer import AbstractSerializer
+from adsorption_database.serializers.attrs_serializer import AttrOnlySerializer
+from adsorption_database.serializers.mix_isotherm_serializer import MixIsothermSerializer
+from adsorption_database.serializers.mono_isotherm_serializer import MonoIsothermSerializer
+from adsorption_database.serializers.shared import get_adsorbent_group_route, get_attr_fields_from_infos, get_root_group
 
 
 class ExperimentSerializer(AbstractSerializer):
@@ -18,23 +24,63 @@ class ExperimentSerializer(AbstractSerializer):
 
     def get_attributes(self):
         return [
-            field.name
+            (field.name, field.type)
             for field in fields(self._model_class)
             if field.type not in [Adsorbent, List[MonoIsotherm], List[MixIsotherm]]
         ]
 
     def load(self, group: Group) -> Any:
 
+        _fields: Dict[str, Any] = {}
         attributes = self.get_attributes()
-        obj = self._model_class()
+        get_attr_fields_from_infos(_fields, attributes, group)
 
-        for attribute in attributes:
-            setattr(obj, attribute, getattr(group.attrs, attribute, None))
+        if "monocomponent_isotherms" in list(group.attrs):
+            _fields["monocomponent_isotherms"] = []
+            for mono_isotherm in group.attrs["monocomponent_isotherms"]:
+                isotherm_group = group.get(mono_isotherm)
+                if isotherm_group is None:
+                    continue
+                mono_isotherm = MonoIsothermSerializer().load(isotherm_group)
+                _fields["monocomponent_isotherms"].append(mono_isotherm)
+
+        if "mixture_isotherms" in list(group.attrs):
+            _fields["mixture_isotherms"] = []
+            for mono_isotherm in group.attrs["mixture_isotherms"]:
+                isotherm_group = group.get(mono_isotherm)
+                if isotherm_group is None:
+                    continue
+                mono_isotherm = MixIsothermSerializer().load(isotherm_group)
+                _fields["mixture_isotherms"].append(mono_isotherm)
+
+        if "adsorbent" in list(group.attrs):
+            root_group = get_root_group(group)
+            adsorbent_group = root_group.get(group.attrs["adsorbent"])
+            if adsorbent_group is not None:
+                _fields["adsorbent"] = AttrOnlySerializer(Adsorbent).load(adsorbent_group)
+            
+
+        obj = self._model_class(**_fields)
 
         return obj
 
-    def dump(self, obj: Any, group: Group) -> None:
+    def dump(self, obj: Experiment, group: Group) -> None:
 
         attributes = self.get_attributes()
+        attribute_names = [attr[0] for attr in attributes]
 
-        self._register_attributes(attributes, obj, group)
+        self._register_attributes(attribute_names, obj, group)
+
+        def get_isotherm_pathnames(isotherms: List[Isotherm], main_path: str) -> List[str]:
+            return [str.encode(main_path + "/" + isotherm.name) for isotherm in isotherms]
+
+        moncomponent_isotherm_names = get_isotherm_pathnames(
+            obj.monocomponent_isotherms, MONO_ISOTHERMS
+        )
+        mixture_isotherms_names = get_isotherm_pathnames(obj.mixture_isotherms, MIXTURE_ISOTHERMS)
+
+        group.attrs["monocomponent_isotherms"] = np.array(moncomponent_isotherm_names)
+        group.attrs["mixture_isotherms"] = np.array(mixture_isotherms_names)
+
+        route = get_adsorbent_group_route(obj.adsorbent.name)
+        group.attrs["adsorbent"] = route
---
+++ adsorption_database/serializers/tests/test_experiment_serializer.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+
+import pytest
+from adsorption_database.defaults import (
+    ADSORBATES,
+    ADSORBENTS,
+    EXPERIMENTS,
+    MIXTURE_ISOTHERMS,
+    MONO_ISOTHERMS,
+)
+from adsorption_database.helpers import Helpers
+from pytest_regressions.data_regression import DataRegressionFixture
+from adsorption_database.models.adsorbate import Adsorbate
+from adsorption_database.models.adsorbent import Adsorbent, AdsorbentType
+from adsorption_database.models.experiment import Experiment, ExperimentType
+from adsorption_database.models.isotherms import MixIsotherm, MonoIsotherm
+from adsorption_database.serializers.abstract_serializer import AbstractSerializer
+from adsorption_database.serializers.attrs_serializer import AttrOnlySerializer
+from adsorption_database.serializers.experiment_serializer import ExperimentSerializer
+from adsorption_database.serializers.mix_isotherm_serializer import MixIsothermSerializer
+from adsorption_database.serializers.mono_isotherm_serializer import MonoIsothermSerializer
+from adsorption_database.storage_provider import StorageProvider
+
+
+@pytest.fixture
+def setup_test_storage(
+    co2_adsorbate: Adsorbate,
+    ch4_adsorbate: Adsorbate,
+    mix_isotherm: MixIsotherm,
+    mono_isotherm: MonoIsotherm,
+    setup_storage: Path,
+) -> Experiment:
+
+    serializer = ExperimentSerializer()
+
+    with StorageProvider().get_editable_file() as f:
+        adsorbates = f.create_group(ADSORBATES)
+        co2_group = adsorbates.create_group(co2_adsorbate.name)
+        ch4_group = adsorbates.create_group(ch4_adsorbate.name)
+        AttrOnlySerializer(Adsorbate).dump(co2_adsorbate, co2_group)
+        AttrOnlySerializer(Adsorbate).dump(ch4_adsorbate, ch4_group)
+        adsorbates = f.create_group(ADSORBENTS)
+        adsorbent = Adsorbent(AdsorbentType.ZEOLITE, "z01x")
+        adsorbent_group = adsorbates.create_group(adsorbent.name, adsorbates)
+        AttrOnlySerializer(Adsorbent).dump(adsorbent, adsorbent_group)
+
+        experiment = Experiment(
+            name="exp-01-02",
+            adsorbent=adsorbent,
+            experiment_type=ExperimentType.GRAVIMETRIC,
+            monocomponent_isotherms=[mono_isotherm],
+            mixture_isotherms=[mix_isotherm],
+            year="1934",
+            authors=["carol", "jhon", "maria"],
+        )
+
+        experiments_group = f.create_group(EXPERIMENTS)
+
+        group = experiments_group.create_group(experiment.name)
+
+        mono_isos = group.create_group(MONO_ISOTHERMS)
+        mono_iso = mono_isos.create_group(mono_isotherm.name)
+        mix_isos = group.create_group(MIXTURE_ISOTHERMS)
+        mix_iso = mix_isos.create_group(mix_isotherm.name)
+
+        MonoIsothermSerializer().dump(mono_isotherm, mono_iso)
+        MixIsothermSerializer().dump(mix_isotherm, mix_iso)
+
+        serializer.dump(experiment, group)
+
+    return experiment
+
+
+def test_dump_experiment(
+    setup_test_storage: None,
+    helpers: Helpers,
+    setup_storage: Path,
+    data_regression: DataRegressionFixture,
+) -> None:
+
+    serialized_tree = helpers.dump_storage_tree(setup_storage)
+
+    data_regression.check({"tree": serialized_tree})
+
+
+def test_load_experiment(helpers: Helpers, setup_test_storage: Experiment) -> None:
+    serializer = ExperimentSerializer()
+
+    with StorageProvider().get_readable_file() as f:
+        obj = serializer.load(f[EXPERIMENTS]["exp-01-02"])
+
+    helpers.assert_equal(setup_test_storage, obj)
